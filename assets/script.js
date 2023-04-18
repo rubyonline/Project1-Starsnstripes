4ee88f0e26285923c576a14e1bfe82dfbe6c4bbf
Ruby <rubyfredrickson@gmail.com>
2023-04-17 19:11:45 -0500

    ignore

---
+++ assets/script.js
@@ -0,0 +1,2 @@
+var artistQuery
+var artistApiUrl = "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" + artistQuery + "&page_size=6&s_track_rating=desc&apikey=3b7e9248b894731f9f881e9b297fd717"
