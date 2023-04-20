d1d8c27db63341692f73674480a4f7b9c55dc404
Ruby <rubyfredrickson@gmail.com>
2023-04-20 18:35:46 -0500

    added ability to view artist name

--- assets/script.js
+++ assets/script.js
@@ -20,20 +20,49 @@
 
 // var searchBtn = document.getElementById("search-button");
 // searchBtn.addEventListener("click", search);
+var artistHeader = document.querySelector("#artist-header")
+var artistParagraph = document.querySelector("#artist-paragraph")
+var artistList = document.querySelector("#artist-list")
 
+var currentArtistID = 0
+
+var lyricHeader = document.querySelector("#lyric-header")
+var lyricParagraph = document.querySelector("#lyric-paragraph")
+var lyricList = document.querySelector("#lyric-list")
 
 function artistSearch(e) {
     e.preventDefault();
     var searchedArtist = document.getElementById("artist-search-input").value;
     var url = `https://api.musixmatch.com/ws/1.1/artist.search?q_artist=${searchedArtist}&apikey=3b7e9248b894731f9f881e9b297fd717`;
-
+  
     fetch(url)
-    .then(response => response.json())
-    .then(function (data) {
-            console.log(data);
+      .then(response => response.json())
+      .then(function (data) {
+        console.log(data);
+        artistHeader.textContent = data.message.body.artist_list[0].artist.artist_name;
+
+        var currentArtistID = data.message.body.artist_list[0].artist.artist_id;
+        console.log(currentArtistID);
+
+        fetch(`https://api.musixmatch.com/ws/1.1/artist.albums.get?artist_id=${currentArtistID}&apikey=3b7e9248b894731f9f881e9b297fd717`)
+        .then(responseTwo => responseTwo.json())
+        .then(function(albumdata){
+            console.log(albumdata)
+
+            for (let i = 0; i < albumdata.message.body.album_list.length; i++) {
+                var albumName = albumdata.message.body.album_list[i].album.album_name;
+                
+                var artistList = document.createElement('li');
+                artistList;
+                artistList.textContent = albumName;
+
+            }
         })
         .catch(error => console.error(error));
-}
+      })
+      .catch(error => console.error(error));
+
+  }
 
 var artistSearchButton = document.getElementById("artist-search-button");
 artistSearchButton.addEventListener("click", artistSearch);
